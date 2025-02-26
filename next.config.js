9cbf6a761e627c7eefb4f18ddd6c4f1809cdc28c
clientesweb <alexisifran32@gmail.com>
2025-02-26 13:52:31 -0300

    Actualizar next.config.js

--- next.config.js
+++ next.config.js
@@ -4,33 +4,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "www.hotelgloria.com.ar",
-          },
-        ],
-        destination: "https://hotelgloria.ar/:path*",
-        permanent: true,
-      },
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "hotelgloria.com.ar",
-          },
-        ],
-        destination: "https://hotelgloria.ar/:path*",
-        permanent: true,
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
-
