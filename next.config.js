ed304e6b4a57ad48fd419d651503d94fc3e4e25a
clientesweb <alexisifran32@gmail.com>
2025-02-26 14:04:18 -0300

    Actualizar next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,22 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  // Add this section to handle multiple domains
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self' https://hotelgloria.ar https://www.hotelgloria.ar https://hotelgloria.com.ar https://www.hotelgloria.com.ar; style-src 'self' 'unsafe-inline' https://hotelgloria.ar https://www.hotelgloria.ar https://hotelgloria.com.ar https://www.hotelgloria.com.ar; script-src 'self' 'unsafe-inline' https://hotelgloria.ar https://www.hotelgloria.ar https://hotelgloria.com.ar https://www.hotelgloria.com.ar;",
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
+
